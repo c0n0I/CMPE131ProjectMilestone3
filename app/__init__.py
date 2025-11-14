10ab95c8132c8c2ece3ad243009d33636357cd0b
Connor <kangruichen431@gmail.com>
2025-11-13 16:39:55 -0800

    Set up SQLAlchemy and LoginManager
    Initialize SQLAlchemy and LoginManager in the app.

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,24 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from .config import Config
+
+db = SQLAlchemy()
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
 
-    # Register blueprints
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    from app import models
+
     from app.main.routes import main_bp
+    from app.auth.routes import auth_bp
+
     app.register_blueprint(main_bp)
+    app.register_blueprint(auth_bp, url_prefix="/auth")
 
     return app
