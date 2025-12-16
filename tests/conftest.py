53f8158f93ee9f7fb581666316f4523c169dee53
Connor <kangruichen431@gmail.com>
2025-12-15 19:48:38 -0800

    Clean up blank lines in tests/conftest.py
    Removed unnecessary blank lines in the conftest.py file.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,7 @@
 import sys
 import os
 
-
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
 import pytest
 from app import create_app, db
 from app.models import User, Folder, Bookmark
